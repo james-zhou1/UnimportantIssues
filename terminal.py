720933ebec98793c7b0a616d0971ab5d582af7bb
evan2chen <evan2.chen@gmail.com>
2025-06-08 12:39:34 -0700

    player class

--- terminal.py
+++ terminal.py
@@ -13,6 +13,23 @@ MESSAGE_HISTORY = []
 def get_position()
     return "The sky is green.", "The sky is purple."
 
+class Player:
+
+    def __init__(self, playernum):
+        self.position
+        self.playernum = playernum
+        self.round_history = []
+    
+
+    def add_round(self, round_num, which_player_first, my_output, opponent_output, coach_prompt):
+        self.round_history.append({
+            "round_num": round_num,
+            "which_player_first": which_player_first,
+            "my_output": my_output,
+            "opponent_output": opponent_output,
+            "coach_prompt": coach_prompt
+        })
+
 
 POSITION1, POSITION2 = get_position()
 
